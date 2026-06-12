8936604d23b118cafc55a495b197162d94a0eec1
Winkesa <winfreywekesa148@gmail.com>
2026-06-12 15:13:43 +0300

    add generate function

---
+++ generate_log.py
@@ -0,0 +1,21 @@
+#import 
+from datetime import datetime
+
+log_data = ["User logged in", "User updated profile", "Report exported"]
+
+#log in
+def __init__ (self, username, password):
+    self.username = username
+    self.password = password
+
+def authenticate():
+    username = input("Enter username: ")
+    password = int(input("Enter password: "))
+
+filename = (f"log_{datetime.now().strftime('%Y%m%d')}.txt")
+
+with open(filename, "w") as file:
+    for entry in log_data:
+        file.write(f"{entry}\n")
+
+print(f"Log written to {filename}")
